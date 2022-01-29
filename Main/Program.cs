6321ed8f4990bf0d4df4550ef4260c166955bd3a
Joan Andres Rincon Garcia <joanandres@gmail.com>
2022-01-28 22:26:09 -0500

    se crea en namesapce la clase Progran, el metodo Main y se crea un remoto  origin con GityHub

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace GitTeams;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
