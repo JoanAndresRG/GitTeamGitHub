c29601aa824d09f9fe57f8e97ae8977a043fdf85
Joan Andres Rincon Garcia <joanandres@gmail.com>
2022-01-28 22:41:50 -0500

    se crea la clase Person con atributos y un metodo Talk, los atributos estan encapsulados

--- Main/Program.cs
+++ Main/Program.cs
@@ -6,6 +6,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        
+
+
+    }
+    //Class Person 
+    public class Person
+    {
+        //Encapsulamiento de atributos de la clase
+        string name  { get; set; }
+        string email { get; set; }
+        int age { get; set; }
+        string address { get; set; }
+        //Metodo de la clase Person
+        public string Talk(string name)
+        {
+            return $"Hello {name} how are you?";
+        }
     }
 }
